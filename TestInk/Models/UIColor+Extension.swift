ece92a83b5b408c8c73e9257adbb68a13dfe6463
Melissa He <melissahe@ac.c4q.nyc>
2018-03-15 13:42:13 -0400

    Add custom colors to UIColor

---
+++ TestInk/Models/UIColor+Extension.swift
@@ -0,0 +1,25 @@
+//
+//  UIColor+Extension.swift
+//  TestInk
+//
+//  Created by C4Q on 3/15/18.
+//  Copyright © 2018 C4Q. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    ///Custom colors we picked out for the app.
+    struct Custom {
+        ///dark blue
+        static var lapisLazuli = UIColor(red: 0.141, green: 0.482, blue: 0.627, alpha: 1)
+        ///white
+        static var whiteSmoke = UIColor(red: 0.95, green: 0.98, blue: 0.96, alpha: 1)
+        ///orange
+        static var mandarin = UIColor(red: 0.922, green: 0.471, blue: 0.247, alpha: 1)
+        ///gray
+        static var gainsboro = UIColor(red: 0.859, green: 0.859, blue: 0.859, alpha: 1)
+        ///light blue
+        static var mediumSkyBlue = UIColor(red: 0.396, green: 0.871, blue: 0.945, alpha: 1.0)
+    }
+}
