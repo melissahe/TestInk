c1457171ca5661073867c296950033b8f7f0fdeb
Izza Nadeem <izza.nadeem99@gmail.com>
2018-03-22 16:24:08 -0400

    added a tab bar button that lets you segue from feedVC to AR

--- TestInk/View Controllers/FeedVC.swift
+++ TestInk/View Controllers/FeedVC.swift
@@ -10,9 +10,44 @@ import UIKit
 
 class FeedVC: UIViewController {
 
+    let feedView = FeedView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .orange
+        view.addSubview(feedView)
+//        feedView.tableView.dataSource = self
+        feedView.tableView.rowHeight = UITableViewAutomaticDimension
+        feedView.tableView.estimatedRowHeight = 120
+        setupViews()
+    }
+    
+    private func setupViews() {
+        //right bar button
+        let uploadButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(uploadButtonPressed))
+        navigationItem.rightBarButtonItem = uploadButton
+        
+    }
+    
+    @objc private func uploadButtonPressed() {
+        let upLoadVC = UploadVC()
+        navigationController?.pushViewController(upLoadVC, animated: true)
     }
 
 }
+
+//extension FeedVC: UITableViewDelegate, UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 10
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "FeedCell", for: indexPath) as! FeedCell
+//
+//        return cell
+//    }
+    
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//
+//    }
+//}
