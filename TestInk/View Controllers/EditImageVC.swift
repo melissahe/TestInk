897e70b11d4c3d64ac2bd68857eb4e90dc5e7e3d
Melissa He <melissahe@ac.c4q.nyc>
2018-03-27 10:26:28 -0400

    Remove AR on/off for now, remove edit image option

--- TestInk/View Controllers/EditImageVC.swift
+++ TestInk/View Controllers/EditImageVC.swift
@@ -15,7 +15,7 @@ class EditImageVC: UIViewController {
     private var pastSliderValue: Float = 0
     private var pastImage: UIImage!
     private var originalImage: UIImage!
-    
+//    private var designID: String?
     private var filters: [(displayName: String, filterName: Filter)] = FilterModel.getFilters()
     
     lazy private var editImageView = EditImageView(frame: view.safeAreaLayoutGuide.layoutFrame)
@@ -43,7 +43,7 @@ class EditImageVC: UIViewController {
         }
         editImageView.photoImageView.image = pastImage
         
-        editImageView.photoOptionsView.resizeButton.addTarget(self, action: #selector(resizeButtonPressed), for: .touchUpInside)
+//        editImageView.photoOptionsView.resizeButton.addTarget(self, action: #selector(resizeButtonPressed), for: .touchUpInside)
         
         editImageView.photoOptionsView.shareButton.addTarget(self, action: #selector(shareButtonPressed), for: .touchUpInside)
         
@@ -54,10 +54,12 @@ class EditImageVC: UIViewController {
     }
     
     private func setUpNavigation() {
-//        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Post", style: .done, target: self, action: #selector(postButtonPressed))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Post to Feed", style: .done, target: self, action: #selector(postButtonPressed))
     }
     
     private func addEditView() {
+        navigationItem.rightBarButtonItem = nil
+        
         navigationItem.leftBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "deleteIcon"), style: .done, target: self, action: #selector(deleteButtonPressed))
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "checkIcon"), style: .done, target: self, action: #selector(saveButtonPressed))
@@ -66,26 +68,31 @@ class EditImageVC: UIViewController {
     private func removeEditView() {
         navigationItem.rightBarButtonItem = nil
         navigationItem.leftBarButtonItem = nil
-        editImageView.editView.isHidden = true
+        setUpNavigation()
+//        editImageView.editView.isHidden = true
         editImageView.filterView.isHidden = true
     }
     
     @objc private func postButtonPressed() {
         //should post to feed?
+        let currentUser = AuthUserService.manager.getCurrentUser()!
+        //DesignID won't work if the user doesn't upload the design they're trying - maybe we should scrap this
+        FirebasePreviewPostService.service.delegate = self
+        FirebasePreviewPostService.service.addPreviewPostToDatabase(userID: currentUser.uid, imageURL: nil, likes: 0, timeStamp: Date.timeIntervalSinceReferenceDate, comments: "", designID: "", flags: 0)
     }
     
-    @objc private func resizeButtonPressed() {
-        editImageView.editView.isHidden = false
-        
-        pastSliderValue = editImageView.editView.sizeSlider.value
-        addEditView()
-    }
+//    @objc private func resizeButtonPressed() {
+//        editImageView.editView.isHidden = false
+//
+//        pastSliderValue = editImageView.editView.sizeSlider.value
+//        addEditView()
+//    }
     
     @objc private func deleteButtonPressed() {
         //do something to track which one you're no longer using
         //reset slider
-        editImageView.editView.sliderValueLabel.text = pastSliderValue.truncatingRemainder(dividingBy: 10).description
-        editImageView.editView.sizeSlider.value = pastSliderValue
+//        editImageView.editView.sliderValueLabel.text = pastSliderValue.truncatingRemainder(dividingBy: 10).description
+//        editImageView.editView.sizeSlider.value = pastSliderValue
         editImageView.photoImageView.image = pastImage
         removeEditView()
     }
@@ -152,5 +159,26 @@ extension EditImageVC: UICollectionViewDataSource {
         let currentFilter = filters[indexPath.row]
             cell.configureCell(withImage: originalImage, andFilter: currentFilter)
         return cell
+    }
+}
+
+extension EditImageVC: PreviewPostDelegate {
+    func didAddPreviewPostToFirebase(_ postService: FirebasePreviewPostService, post: PreviewPost) {
+        //success
+        let successAlert = Alert.create(withTitle: "Success", andMessage: "Posted tattoo preview to feed.", withPreferredStyle: .alert)
+        Alert.addAction(withTitle: "OK", style: .default, andHandler: nil, to: successAlert)
+        self.present(successAlert, animated: true, completion: nil)
+    }
+    
+    func failedToAddPreviewPostToFirebase(_ postService: FirebasePreviewPostService, error: Error) {
+        //error
+        let errorAlert = Alert.createErrorAlert(withMessage: "Could not add tattoo preview to feed.")
+        self.present(errorAlert, animated: true, completion: nil)
+    }
+    
+    func didGetAllPreviewPosts(_ postService: FirebasePreviewPostService, post: PreviewPost) {
+    }
+    
+    func failedToGetAllPreviewPosts(_ postService: FirebasePreviewPostService, error: Error) {
     }
 }
