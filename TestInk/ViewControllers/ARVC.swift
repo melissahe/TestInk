73c32b6b79206db125771c22535cc0df2d6f1dc0
Nicole Souvenir <github@nicolesouvenir.com>
2018-03-27 11:41:48 -0400

    tweaked storage functions, refactored function in EditImage VC, changed folder names to get rid of spaces

--- TestInk/ViewControllers/ARVC.swift
+++ TestInk/ViewControllers/ARVC.swift
@@ -76,12 +76,12 @@ class ARVC: UIViewController {
         //this configuration uses the back camera and tracks device's movement
         let configuration = ARWorldTrackingConfiguration()
         //load the set of reference images
-        guard let referenceImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil) else {
-            print("Missing asset images")
-            return
-        }
+//        guard let referenceImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil) else {
+//            print("Missing asset images")
+//            return
+//        }
         //tell the AR configuration to detect these reference images
-        configuration.detectionImages = referenceImages
+        //configuration.detectionImages = referenceImages
         //run the scene
         //if the scene is paused, it will remove all nodes and stop tracking
         sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
@@ -145,7 +145,7 @@ class ARVC: UIViewController {
     }
 
     private func setUpViews() {
-        sceneView.delegate = self
+       // sceneView.delegate = self
         sceneView.allowsCameraControl = true
         sceneView.showsStatistics = true
         arView.captureButton.addTarget(self, action: #selector(captureButtonPressed), for: .touchUpInside)
@@ -198,49 +198,50 @@ class ARVC: UIViewController {
 
 }
 
-extension ARVC: ARSCNViewDelegate {
-    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-        guard let imageAnchor = anchor as? ARImageAnchor
-            //        let planeAnchor = anchor as? ARPlaneAnchor
-            else {
-                print("couldn't get image anchor")
-                return
-        }
-        
-        let referenceImage = imageAnchor.referenceImage
-        updateQueue.async {
-            //maybe i could let user add in what size they want the image to be? and use that instead?
-                //figure out how to let user interact with the node
-            let image = self.tattooImage!
-            //points to meters = (x points * 0.000352778
-            let imageWidthMeters = image.size.width * 0.000352778
-            let imageHeightMeters = image.size.height * 0.000352778
-            //find the smallest scale, to scale uiimage down to size of reference
-            let scaleWidth = (referenceImage.physicalSize.width < imageWidthMeters) ? referenceImage.physicalSize.width / imageWidthMeters : imageWidthMeters / referenceImage.physicalSize.width
-            let scaleHeight = (referenceImage.physicalSize.height < imageHeightMeters) ? referenceImage.physicalSize.height / imageHeightMeters : imageHeightMeters / referenceImage.physicalSize.height
-            //use the smaller scale
-            let scale = (scaleWidth > scaleHeight) ? scaleHeight : scaleWidth
-            
-            //create the plane for the initial position of the detected image
-            let plane = SCNPlane(width: imageWidthMeters * scale, height: imageHeightMeters * scale)
-            let material = SCNMaterial()
-            material.diffuse.contents = image
-            plane.materials = [material]
-            let planeNode = SCNNode(geometry: plane)
-            planeNode.opacity = 0.75
-            
-            //SCNPlane is vertically oriented by default, but imageAnchor is horizontally orientated, but ARImageAnchor assumes the image is horizontal, so rotate the plane to match
-            planeNode.transform = SCNMatrix4MakeRotation(-Float.pi / 2, 1, 0, 0)
-            
-            node.addChildNode(planeNode)
-            //remove previous node in case multiple ones are added
-            if let previousNode = self.currentNode, let anchor = self.sceneView.anchor(for: previousNode) {
-                self.sceneView.session.remove(anchor: anchor)
-                //should get rid of the current node
-                previousNode.removeFromParentNode()
-            }
-            //add current node
-            self.currentNode = planeNode
-        }
-    }
-}
+//extension ARVC: ARSCNViewDelegate {
+//    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+//        guard let imageAnchor = anchor as? ARImageAnchor
+//            //        let planeAnchor = anchor as? ARPlaneAnchor
+//            else {
+//                print("couldn't get image anchor")
+//                return
+//        }
+//
+//        let referenceImage = imageAnchor.referenceImage
+//        updateQueue.async {
+//            //maybe i could let user add in what size they want the image to be? and use that instead?
+//                //figure out how to let user interact with the node
+//            let image = self.tattooImage!
+//            //points to meters = (x points * 0.000352778
+//            let imageWidthMeters = image.size.width * 0.000352778
+//            let imageHeightMeters = image.size.height * 0.000352778
+//            //find the smallest scale, to scale uiimage down to size of reference
+//            let scaleWidth = (referenceImage.physicalSize.width < imageWidthMeters) ? referenceImage.physicalSize.width / imageWidthMeters : imageWidthMeters / referenceImage.physicalSize.width
+//            let scaleHeight = (referenceImage.physicalSize.height < imageHeightMeters) ? referenceImage.physicalSize.height / imageHeightMeters : imageHeightMeters / referenceImage.physicalSize.height
+//            //use the smaller scale
+//            let scale = (scaleWidth > scaleHeight) ? scaleHeight : scaleWidth
+//
+//            //create the plane for the initial position of the detected image
+//            let plane = SCNPlane(width: imageWidthMeters * scale, height: imageHeightMeters * scale)
+//            let material = SCNMaterial()
+//            material.diffuse.contents = image
+//            plane.materials = [material]
+//            let planeNode = SCNNode(geometry: plane)
+//            planeNode.opacity = 0.75
+//
+//            //SCNPlane is vertically oriented by default, but imageAnchor is horizontally orientated, but ARImageAnchor assumes the image is horizontal, so rotate the plane to match
+//            planeNode.transform = SCNMatrix4MakeRotation(-Float.pi / 2, 1, 0, 0)
+//
+//            node.addChildNode(planeNode)
+//            //remove previous node in case multiple ones are added
+//            if let previousNode = self.currentNode, let anchor = self.sceneView.anchor(for: previousNode) {
+//                self.sceneView.session.remove(anchor: anchor)
+//                //should get rid of the current node
+//                previousNode.removeFromParentNode()
+//            }
+//            //add current node
+//            self.currentNode = planeNode
+//        }
+//    }
+//}
+
