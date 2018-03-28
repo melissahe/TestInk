c71ae92a179e84955e1495fdf9e962dd394572fb
Nicole Souvenir <github@nicolesouvenir.com>
2018-03-28 08:40:22 -0400

    flagging functions and delegates complete and ready for testing

--- TestInk/Service/Database/FirebaseFlaggingService.swift
+++ TestInk/Service/Database/FirebaseFlaggingService.swift
@@ -28,16 +28,7 @@ class FirebaseFlaggingService{
     
     static let service = FirebaseFlaggingService()
     weak var delegate: FlagDelegate?
-    
-    
-    //TODO: complete add function
-    
-    /*MARK:
-     -Add flag to database
-     -Add flag to user who is flagged
-     -Add flag to the post being flagged
-     */
-    
+
     //MARK: Adding a flag under the flag node in firebase
     func addFlagToFirebase(flagID: String, userID: String, flaggedBy: String, userFlagged: String, postID: String, flagMessage: String ){
         //create unique identifier
@@ -49,15 +40,18 @@ class FirebaseFlaggingService{
         //set that value of the data converted by json
         childByAutoID.setValue(flags.flagsToJSON()) { (error, dbRef) in
             if let error = error {
-                self.delegate?.failedToAddFlagToFirebase()
+                self.delegate?.failedToAddFlagToFirebase(self, error: error.localizedDescription)
                 print("Error adding flag to firebase: \(error.localizedDescription)")
                 print("Flag not added to fb")
             } else {
-                self.delegate?.didAddFlagToFirebase()
+                self.delegate?.didAddFlagToFirebase(self)
                 print("Flag added to firebase with flagID: \(flagID)")
             }
         }
     }
+    
+  
+    ////TODO: Refactor below functions to model after codable data
     
     
     //MARK: Flagging a post and preventing the user from flagging the post more than once
@@ -72,13 +66,13 @@ class FirebaseFlaggingService{
                 var flags = designPost["flags"] as? Int ?? 0
                 //if user has flagged before
                 if let _ = flaggedByDict[userID] {
-                    //self.delegate?.didFlagPostAlready?(self, error: "You have flagged this post already.")
+                    self.delegate?.didFlagPostAlready(self, error: "You have flagged this post already.")
                 } else { //user has not flagged before
                     // flag the post and add flag to self for tracking
                     flaggedByDict[userID] = true
                     flags += 1
                     DispatchQueue.main.async {
-                        //self.delegate?.didFlagPost?(self)
+                        self.delegate?.didFlagPost(self)
                         flaggedCompletion(true)
                     }
                 }
@@ -92,7 +86,7 @@ class FirebaseFlaggingService{
             return TransactionResult.success(withValue: currentData)
         }, andCompletionBlock: { (error, bool, _) in
             if let error = error {
-                //self.delegate?.didFailFlagging?(self, error: error.localizedDescription)
+                self.delegate?.didFailToFlagPost(self, error: error.localizedDescription)
                 print("error flagging post: \(error.localizedDescription)")
             }
         })
@@ -117,23 +111,15 @@ class FirebaseFlaggingService{
                     favorites -= 1
                     favoritesDict.removeValue(forKey: userID)
                     DispatchQueue.main.async {
-                        //self.delegate?.didUndoLikePost?(self, withPostID: postID)
+                        self.delegate?.didUnfavoritePost(self, withPostID: designPostID)
                         favoriteCompletion(favorites)
                     }
                 } else { //if user has not liked yet
                     //favorite the post and add self to favorites
                     favorites += 1
                     favoritesDict[userID] = true
-                    //                    if let _ = dislikesDict[userID] {
-                    //                        dislikes -= 1
-                    //                        dislikesDict.removeValue(forKey: userID)
-                    //                        DispatchQueue.main.async {
-                    //                            self.delegate?.didUndoDislikePost?(self, withPostID: postID)
-                    //                            dislikeCompletion(dislikes)
-                    //                        }
-                    //                    }
                     DispatchQueue.main.async {
-                        //self.delegate?.didLikePost?(self, withPostID: postID)
+                        self.delegate?.didFavoritePost(self, withPostID: designPostID)
                         favoriteCompletion(favorites)
                     }
                 }
@@ -148,7 +134,7 @@ class FirebaseFlaggingService{
         }, andCompletionBlock: { (error, _, _) in
             if let error = error {
                 //delegate with fail to like post
-                //self.delegate?.didFailLiking?(self, error: error.localizedDescription)
+                self.delegate?.didFailFavoritingPost(self, error: error.localizedDescription)
                 print("error favoriting post: \(error.localizedDescription)")
             }
         })
@@ -174,7 +160,6 @@ class FirebaseFlaggingService{
                 if let _ = flaggedByDict[userID] {
                     //self.delegate?.didFlagUserAlready?(self, error: "You have flagged this user already.")
                 } else { //user has not flagged before
-                    
                     // favorite the post and add self to favorites
                     flaggedByDict[userID] = true
                     flags += 1
