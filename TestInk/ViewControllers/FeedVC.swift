8ab599bcd4a8ae443be975cd4e0e76e67fe60c61
Nicole Souvenir <github@nicolesouvenir.com>
2018-04-02 18:33:44 -0400

    Added segmented control to go between design and preview tabs. Workingon adding underlining to segmented control

--- TestInk/ViewControllers/FeedVC.swift
+++ TestInk/ViewControllers/FeedVC.swift
@@ -18,7 +18,7 @@ class FeedVC: UIViewController {
         return AuthUserService.manager.getCurrentUser()!.uid
     }
     private var refreshControl: UIRefreshControl!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
@@ -28,10 +28,17 @@ class FeedVC: UIViewController {
         feedView.designTableView.refreshControl = refreshControl
         feedView.designTableView.delegate = self
         feedView.designTableView.dataSource = self
+        //MARK: used for self sizing cells
         feedView.designTableView.rowHeight = UITableViewAutomaticDimension
         feedView.designTableView.estimatedRowHeight = 200
         self.title = "Feed"
+        //MARK: functionality for segmented control
+        //feedView.segmentedControl.addTarget(self, action: #selector(segControlIndexPressed(_ sender: UISegmentedControl)), for: .normal)
     }
+    
+//    @objc private func segControlIndexPressed(_ sender : UISegmentedControl){
+//        print("segmented control working")
+//    }
     
     @objc private func tableViewRefreshed() {
         loadData()
@@ -101,6 +108,7 @@ extension FeedVC: UITableViewDataSource{
             let cell = tableView.dequeueReusableCell(withIdentifier: "FeedCell") as! FeedCell
             let currentDesign = designPosts[indexPath.row]
             cell.delegate = self
+            cell.selectionStyle = .none
             cell.configureCell(withPost: currentDesign)
             return cell
         }
@@ -220,5 +228,57 @@ extension FeedVC: LikeServiceDelegate {
     }
     
     func didFailFavoritingPost(_ service: FirebaseLikingService, error: String) {
+    }
+}
+
+//////////////////MARK: this will remove the the border from the segmented control and add an underline for the selected segment
+//inspiration: https://stackoverflow.com/questions/42755590/how-to-display-only-bottom-border-for-selected-item-in-uisegmentedcontrol
+extension UISegmentedControl{
+    
+    func removeBorder(){
+        let backgroundImage = UIImage.getColoredRectImageWith(color: UIColor.white.cgColor, andSize: self.bounds.size)
+        self.setBackgroundImage(backgroundImage, for: .normal, barMetrics: .default)
+        self.setBackgroundImage(backgroundImage, for: .selected, barMetrics: .default)
+        self.setBackgroundImage(backgroundImage, for: .highlighted, barMetrics: .default)
+        
+        let deviderImage = UIImage.getColoredRectImageWith(color: UIColor.white.cgColor, andSize: CGSize(width: 1.0, height: self.bounds.size.height))
+        self.setDividerImage(deviderImage, forLeftSegmentState: .selected, rightSegmentState: .normal, barMetrics: .default)
+        self.setTitleTextAttributes([NSAttributedStringKey.foregroundColor: UIColor.gray], for: .normal)
+        self.setTitleTextAttributes([NSAttributedStringKey.foregroundColor: UIColor(red: 67/255, green: 129/255, blue: 244/255, alpha: 1.0)], for: .selected)
+    }
+    
+    func addUnderlineForSelectedSegment(){
+        removeBorder()
+        let underlineWidth: CGFloat = self.bounds.size.width / CGFloat(self.numberOfSegments)
+        let underlineHeight: CGFloat = 2.0
+        let underlineXPosition = CGFloat(selectedSegmentIndex * Int(underlineWidth))
+        let underLineYPosition = self.bounds.size.height - 1.0
+        let underlineFrame = CGRect(x: underlineXPosition, y: underLineYPosition, width: underlineWidth, height: underlineHeight)
+        let underline = UIView(frame: underlineFrame)
+        underline.backgroundColor = UIColor(red: 67/255, green: 129/255, blue: 244/255, alpha: 1.0)
+        underline.tag = 1
+        self.addSubview(underline)
+    }
+    
+    func changeUnderlinePosition(){
+        guard let underline = self.viewWithTag(1) else {return}
+        let underlineFinalXPosition = (self.bounds.width / CGFloat(self.numberOfSegments)) * CGFloat(selectedSegmentIndex)
+        UIView.animate(withDuration: 0.1, animations: {
+            underline.frame.origin.x = underlineFinalXPosition
+        })
+    }
+}
+
+extension UIImage{
+    
+    class func getColoredRectImageWith(color: CGColor, andSize size: CGSize) -> UIImage{
+        UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
+        let graphicsContext = UIGraphicsGetCurrentContext()
+        graphicsContext?.setFillColor(color)
+        let rectangle = CGRect(x: 0.0, y: 0.0, width: size.width, height: size.height)
+        graphicsContext?.fill(rectangle)
+        let rectangleImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return rectangleImage!
     }
 }
